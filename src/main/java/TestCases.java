48db243ef6f93d74d3270fcd27b3dbde7ab69d08
Kajal Chaudhary <kajal.chaudhary@ACKO620.local>
2022-07-29 03:07:25 +0530

    Initial commit

---
+++ src/main/java/TestCases.java
@@ -0,0 +1,14 @@
+import org.testng.annotations.Test;
+
+public class TestCases {
+
+    @Test
+    void test1(){
+        System.out.println("Inside test1");
+    }
+
+    @Test
+    void test2(){
+        System.out.println("Inside test2");
+    }
+}
